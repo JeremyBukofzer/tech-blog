af6aaf7465f68503d8492fb64fba61d1b1d9d8bf
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-12-08 15:52:22 -0600

    adds backend

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,81 @@
+const router = require('express').router();
+const { User, Blog, Comment } = require('../models');
+const withAuth = require('..utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [{
+                model: User,
+                attributes: ['username'],
+            },],
+        });
+        const blogs = blogData.map((blog) => blog.get({
+            plain: true
+        }));
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPK(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment, 
+                    include: [
+                        User
+                    ]
+                }
+            ],
+        });
+        const blog = blogData.get({
+            plain: true
+        });
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPK(req.session.user_id, {
+            attribuutes: {
+                exclude: ['password']
+            },
+            include: [{
+                model: Blog
+            }],
+        });
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('signUp');
+});
+
+module.exports = router;
