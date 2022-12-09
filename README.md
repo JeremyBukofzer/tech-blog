20c0e4099436d3bc43afccf229de93b7aee1ee18
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-12-08 22:21:59 -0600

    adds readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# The Tech Blog
+
+  ## Description
+
+  A platform in which a user can create an account and post comments to a page.
+
+
+
+  ## Screenshot
+
+  ![screenshot](images/tbscreenshot.png)
+
+
+  
+
+  ## Questions
+   - [GitHub:JeremyBukofzer](https://github.com/JeremyBukofzer)
+   - [Email](jlbukofzer@gmail.com)
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,79 +3,95 @@ const { User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        const blogData = await Blog.findAll({
-            include: [{
-                model: User,
-                attributes: ['username'],
-            },],
-        });
-        const blogs = blogData.map((blog) => blog.get({
-            plain: true
-        }));
-        res.render('homepage', {
-            blogs,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		const blogData = await Blog.findAll({
+			include: [{
+				model: User,
+				attributes: ['username'],
+			},],
+		});
+
+		const blogs = blogData.map((blog) => blog.get({
+			plain: true
+		}));
+
+		res.render('homepage', {
+			blogs,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/blog/:id', async (req, res) => {
-    try {
-        const blogData = await Blog.findByPK(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-                {
-                    model: Comment, 
-                    include: [
-                        User
-                    ]
-                }
-            ],
-        });
-        const blog = blogData.get({
-            plain: true
-        });
-        res.render('blog', {
-            ...blog,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		const blogData = await Blog.findByPk(req.params.id, {
+			include: [
+				{
+					model: User,
+					attributes: ['username'],
+				}, {
+					model: Comment,
+					include: [
+						User
+					]
+				}
+			],
+		});
+
+		const blog = blogData.get({
+			plain: true
+		});
+
+		res.render('blog', {
+			...blog,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-    try {
-        const userData = await User.findByPK(req.session.user_id, {
-            attribuutes: {
-                exclude: ['password']
-            },
-            include: [{
-                model: Blog
-            }],
-        });
-        res.render('dashboard', {
-            ...user,
-            logged_in: true
-        });
+	try {
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: {
+				exclude: ['password']
+			},
+			include: [{
+				model: Blog
+			}],
+		});
 
-    } catch (err) {
-        res.status(500).json(err);
-    }
+		const user = userData.get({
+			plain: true
+		});
+
+		res.render('dashboard', {
+			...user,
+			logged_in: true
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('signup');
+	if (req.session.logged_in) {
+		res.redirect('/dashboard');
+		return;
+	}
+
+	res.render('login');
+});
+
+router.get('/signUp', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/dashboard');
+		return;
+	}
+	res.render('signup');
 });
 
 module.exports = router;
